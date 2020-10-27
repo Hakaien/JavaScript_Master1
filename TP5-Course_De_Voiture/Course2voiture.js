5c14c2f1844265f87a08c9d9c49463cd3015fa22
DWWM <DWWM@DL07.exchange.ad.afpanet>
2020-10-27 17:36:11 +0100

    vroom vroom

--- TP5-Course_De_Voiture/Course2voiture.js
+++ TP5-Course_De_Voiture/Course2voiture.js
@@ -1,22 +1,66 @@
 $(window).ready(function () {
 
-    $("#btnGo").on("click", function () {
-        alert('test');
-    })
+    var nIntervId;
+    // définition du constructeur pour une voiture
+    class Voiture {
+        constructor(id, x, y, img) {
+            this.id = id;
+            this.x = x;
+            this.y = y;
+            this.img = img;
+            this.dessiner(id, img);
+        }
 
-    class Voiture = function (x, y, img) {
-        this.x = x;
-        this.y = y;
-        this.dessiner(img);
     };
-
-    var car1 = new Voiture(1000, 105, "img/car1.png");
-    var car2 = new Voiture(1000, 280, "img/car2.png");
-
-    Voiture.prototype.dessiner = function (img) {
-        // Placez js ici
+        // "Méthode" de dessin
+        Voiture.prototype.dessiner = function (id, img) {
+        let elt = "<img id="+id+" src=" + img + ">";
+        $("#car").append(elt);
     }
 
-  
+    var car1 = new Voiture("car1", 1, 130, "img/car1.png");
+    var car2 = new Voiture("car2", 1, 360, "img/car2.png");
 
-})
+    // Positionnement initial des voitures
+    $("#car1").css({ position: "absolute", right: car1.x, top: car1.y });
+    $("#car2").css({ position: "absolute", right: car2.x, top: car2.y });
+
+    // Génère un random de 0 à 15 
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+
+    // Méthode de déplacement des voitures
+    function deplacerAGauche(distance) {
+        car1.x += getRandomInt(15);
+        car2.x += getRandomInt(15);
+        $("#car1").css({ position: "absolute", right: car1.x, top: car1.y });
+        $("#car2").css({ position: "absolute", right: car2.x, top: car2.y });
+    }
+
+    // !!!!!!!!!!!!
+    // boutton "GO"
+    $("#btnGo").on("click", function () {
+        nIntervId = setInterval(deplacerAGauche, [100]);
+    })
+
+
+    // ---------------------------
+    // fonction d'arrêt
+    function arretDeplacer() {
+        clearInterval(nIntervId);
+    }
+    // Boutton stop
+    $("#btnStop").on("click", function () {
+        arretDeplacer();
+    })
+
+    // Boutton remise à zéro des voitures
+    $("#btnReset").on("click", function () {
+        arretDeplacer();
+        car1.x = 1;
+        car2.x = 1;
+        $("#car1").css({ position: "absolute", right: car1.x, top: car1.y });
+        $("#car2").css({ position: "absolute", right: car2.x, top: car2.y });
+    })
+})                        
