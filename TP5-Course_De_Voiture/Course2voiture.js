bd0ca26a4522d00a0993f37ccc8770297dd5f647
Hakaien <kahin54@gmail.com>
2020-10-28 13:41:44 +0100

    fin voiture

--- TP5-Course_De_Voiture/Course2voiture.js
+++ TP5-Course_De_Voiture/Course2voiture.js
@@ -1,6 +1,6 @@
 $(window).ready(function () {
 
-    var nIntervId;
+    var nIntervId=null;
     // définition du constructeur pour une voiture
     class Voiture {
         constructor(id, x, y, img) {
@@ -14,7 +14,7 @@ $(window).ready(function () {
     };
         // "Méthode" de dessin
         Voiture.prototype.dessiner = function (id, img) {
-        let elt = "<img id="+id+" src=" + img + ">";
+        let elt = "<img id=" + id + " src=" + img + ">";
         $("#car").append(elt);
     }
 
@@ -22,33 +22,56 @@ $(window).ready(function () {
     var car2 = new Voiture("car2", 1, 360, "img/car2.png");
 
     // Positionnement initial des voitures
-    $("#car1").css({ position: "absolute", right: car1.x, top: car1.y });
-    $("#car2").css({ position: "absolute", right: car2.x, top: car2.y });
+    $("#car1").css({ position: "absolute", right: car1.x, top: car1.y, });
+    $("#car2").css({ position: "absolute", right: car2.x, top: car2.y, });
 
     // Génère un random de 0 à 15 
     function getRandomInt(max) {
         return Math.floor(Math.random() * Math.floor(max));
     }
 
-    // Méthode de déplacement des voitures
-    function deplacerAGauche(distance) {
-        car1.x += getRandomInt(15);
-        car2.x += getRandomInt(15);
-        $("#car1").css({ position: "absolute", right: car1.x, top: car1.y });
-        $("#car2").css({ position: "absolute", right: car2.x, top: car2.y });
-    }
+
+
 
     // !!!!!!!!!!!!
     // boutton "GO"
     $("#btnGo").on("click", function () {
-        nIntervId = setInterval(deplacerAGauche, [100]);
-    })
 
+        // Méthode de déplacement des voitures
+        if (nIntervId==null){
+            nIntervId = setInterval(function () {            
+                car1.x += getRandomInt(15);
+                car2.x += getRandomInt(15);
+                $("#car1").css({ position: "absolute", right: car1.x, top: car1.y, left: "" });
+                $("#car2").css({ position: "absolute", right: car2.x, top: car2.y, left: "" });
+
+            //récupération de l'élément left puis split pour avoir une valeure numérique sans décimale.
+            let car1Arr = $("#car1").css("left");
+            let car2Arr = $("#car2").css("left");
+            let temp = car1Arr.split(".");
+            let temp2 = car2Arr.split(".");
+                // condition de victoire car1
+                if (temp[0] <= 50) {
+                    clearInterval(nIntervId);
+                    alert("Rouge a gagné");
+                }
+                // condition de victoire car2
+                else if (temp2[0] <= 50) {
+                    clearInterval(nIntervId);
+                    alert("Bleu a gagné");
+                }
+        
+            }, 100)
+        }
+    })
 
     // ---------------------------
     // fonction d'arrêt
     function arretDeplacer() {
+        if (nIntervId!=null){
         clearInterval(nIntervId);
+        nIntervId=null;
+        }
     }
     // Boutton stop
     $("#btnStop").on("click", function () {
@@ -63,4 +86,4 @@ $(window).ready(function () {
         $("#car1").css({ position: "absolute", right: car1.x, top: car1.y });
         $("#car2").css({ position: "absolute", right: car2.x, top: car2.y });
     })
-})                        
+})
